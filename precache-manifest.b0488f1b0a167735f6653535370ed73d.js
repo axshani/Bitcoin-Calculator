91a483362acf053ff0668b8474a8ac6b2f3251e7
axshani <aalexshani@gmail.com>
2021-05-28 09:04:15 +0300

    Updates

---
+++ precache-manifest.b0488f1b0a167735f6653535370ed73d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241c1930b7555d1c9ca2b555e584894a",
+    "url": "/Bitcoin-Calculator/index.html"
+  },
+  {
+    "revision": "9efd5ad80cb781c86444",
+    "url": "/Bitcoin-Calculator/static/css/main.100609f3.chunk.css"
+  },
+  {
+    "revision": "a464cc7211f976c98d48",
+    "url": "/Bitcoin-Calculator/static/js/2.24eb0e75.chunk.js"
+  },
+  {
+    "revision": "5c0eb14f6adce81adc82242314745973",
+    "url": "/Bitcoin-Calculator/static/js/2.24eb0e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9efd5ad80cb781c86444",
+    "url": "/Bitcoin-Calculator/static/js/main.aa05f9fc.chunk.js"
+  },
+  {
+    "revision": "d66e080d38c82b4c1c08",
+    "url": "/Bitcoin-Calculator/static/js/runtime-main.ed35ca1e.js"
+  }
+]);
